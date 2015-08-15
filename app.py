1bb07a8536a96b898d29e3af0e2841a6edc71c1f
ycrichard <ycrichard330@gmail.com>
2015-08-15 17:49:15 +0200

    ip

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # all the imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 try:
     from configparser import ConfigParser
 except ImportError:
@@ -12,7 +12,7 @@ try:
 	config.read('logviewer.ini')
 	# read values from a section
 	logfile = config.get('DEFAULT', 'logfile')
-	ip = config.get('DEFAULT', 'ip')
+	# ip = config.get('DEFAULT', 'ip')
 except:
 	from initial import config_gen
 	config_gen()
@@ -21,13 +21,16 @@ except:
 app = Flask(__name__)
 
 @app.route('/viewlog')
-def show_log():
+def show_log():	
     with open(logfile,'r') as f:
-        # data = f.read().split('\n')
-        data = f.readlines()
+    	data = f.readlines()
+    # get ip address from request user
+    ip = request.remote_addr
     results = [ line for line in data if line.find(ip) >=0 ]
     results.reverse()	# reverse the timeline order
-    return render_template('logs.html', logs=results)
+    # user agent
+    UA_string  = request.user_agent.string 
+    return render_template('logs.html', logs=results, ip=ip, UA=UA_string, len=len(results))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
