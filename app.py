bf03ebe49abb665a83fce421abc8baf37322de9f
ycrichard <ycrichard330@gmail.com>
2015-08-15 20:08:47 +0200

    UA

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # all the imports
 from flask import Flask, render_template, request
+import geoip2.database
 try:
     from configparser import ConfigParser
 except ImportError:
@@ -12,25 +13,36 @@ try:
 	config.read('logviewer.ini')
 	# read values from a section
 	logfile = config.get('DEFAULT', 'logfile')
-	# ip = config.get('DEFAULT', 'ip')
+	ip = config.get('DEFAULT', 'ip')
 except:
 	from initial import config_gen
 	config_gen()
 
+reader = geoip2.database.Reader('static/GeoLite2-City.mmdb')
 
 app = Flask(__name__)
 
 @app.route('/viewlog')
-def show_log():	
-    with open(logfile,'r') as f:
+def show_log():
+	# get ip address from request user
+    # ip = request.remote_addr
+	# user agent
+    UA_string  = request.user_agent.string 
+    try:
+    	response = reader.city(ip)
+    	location = response.city.names['zh-CN'] +', '+ response.country.names['zh-CN']
+    except:
+    	location = 'unknow location'
+
+    # get log file
+    with open(logfile,'rb') as f:
     	data = f.readlines()
-    # get ip address from request user
-    ip = request.remote_addr
+    
     results = [ line for line in data if line.find(ip) >=0 ]
     results.reverse()	# reverse the timeline order
-    # user agent
-    UA_string  = request.user_agent.string 
-    return render_template('logs.html', logs=results, ip=ip, UA=UA_string, len=len(results))
+    
+    return render_template('logs.html', logs=results, ip=ip, 
+    	loc=location, UA=UA_string, len=len(results))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
