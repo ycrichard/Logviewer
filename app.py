11c4fadfa2f9cd1deaee3cbdfce9703fd9702773
ycrichard <ycrichard330@gmail.com>
2015-08-15 23:57:57 +0200

    new layout

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # all the imports
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, request
 import geoip2.database
 try:
     from configparser import ConfigParser
@@ -13,7 +13,7 @@ try:
 	config.read('logviewer.ini')
 	# read values from a section
 	logfile = config.get('DEFAULT', 'logfile')
-	ip = config.get('DEFAULT', 'ip')
+	# ip = config.get('DEFAULT', 'ip')
 except:
 	from initial import config_gen
 	config_gen()
@@ -22,10 +22,19 @@ reader = geoip2.database.Reader('static/GeoLite2-City.mmdb')
 
 app = Flask(__name__)
 
+def split_logline(line):
+    segments = line.split()
+    newline = ['','','']
+    newline[0] = ' '.join(segments[0:2])
+    newline[1] = segments[2]
+    newline[2] = ' '.join(segments[3:])
+    return newline
+
+
 @app.route('/viewlog')
-def show_log():
+def viewlog():
 	# get ip address from request user
-    # ip = request.remote_addr
+    ip = request.remote_addr
 	# user agent
     UA_string  = request.user_agent.string 
     try:
@@ -38,11 +47,31 @@ def show_log():
     with open(logfile,'rb') as f:
     	data = f.readlines()
     
-    results = [ line for line in data if line.find(ip) >=0 ]
+    results = [ split_logline(line) for line in data if line.find(ip) >=0 ]
     results.reverse()	# reverse the timeline order
     
     return render_template('logs.html', logs=results, ip=ip, 
-    	loc=location, UA=UA_string, len=len(results))
+    	loc=location, UA=UA_string)
+
+@app.route('/fulllog')
+def fulllog():
+    # get ip address from request user
+    ip = request.remote_addr
+    # user agent
+    UA_string  = request.user_agent.string 
+    try:
+        response = reader.city(ip)
+        location = response.city.names['zh-CN'] +', '+ response.country.names['zh-CN']
+    except:
+        location = 'unknow location'
+
+    # get log file
+    with open(logfile,'rb') as f:
+        data = f.readlines()
+    results = [ split_logline(line) for line in data]
+    results.reverse()   # reverse the timeline order 
+    return render_template('logs_full.html', logs=results, ip=ip, 
+        loc=location, UA=UA_string)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
